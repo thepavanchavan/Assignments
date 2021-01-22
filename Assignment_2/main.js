69425c763e99cc0ac3e8c56e32deb1187a9129aa
thepavanchavan <pavanchavan2060@gmail.com>
2021-01-22 19:24:35 +0530

    Functionality: Add, Delete, Search

--- Assignment_2/main.js
+++ Assignment_2/main.js
@@ -1,51 +1,169 @@
 var information = [
-    {'name': 'John', 'city': 'Mumbai' },
-    {'name': 'Leo', 'city': 'Delhi' },
-    {'name': 'Ram', 'city': 'Pune' },
-    {'name': 'Sham', 'city': 'Nashik' },
-    {'name': 'Johny', 'city': 'Kolkata' },
+{'id':'11','name': 'John', 'city': 'Mumbai','branch':'CSE','degree': 'BTech' },
+{'id':'22','name': 'Leo', 'city': 'Delhi', 'branch':'Mechanical','degree': 'MTech' },
+{'id':'33','name': 'Ram', 'city': 'Pune', 'branch':'CSE','degree': 'PHD' },
+{'id':'44','name': 'Julia', 'city': 'Nashik', 'branch':'IT','degree': 'BCS' },
+{'id':'55','name': 'Johny', 'city': 'Kolkata', 'branch':'Aerospace','degree': 'MS' }
 ]
 
-buildTable(information)
+showTable();
 
-function buildTable(data) {
-    var table = document.getElementById('myTable')
-
-    for (var i = 0; i < data.length; i++){
-        var row = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].city}</td>
-                        <td><button>Edit</button></td>
-                        <td><button onclick="deleteRow(this)">Delete</button></td>
-                  </tr>`
-        table.innerHTML += row
+// FUNCTION FOR ADDING NEW 
 
 
-    }
+function addRow(){
+var id = document.querySelector(".id-form").value
+var name = document.querySelector(".name-form").value;
+var city = document.querySelector(".city-form").value;
+var branch = document.querySelector(".dept-form").value;
+var degree = document.querySelector(".degree-form").value;
+console.log(id,name,city,branch,degree)
+information.push({"id":id, "name":name, "city":city,"branch":branch, "degree":degree});
+
+showTable()
 }
 
-function searchFunction(){
-    var input, filter, table, tableRow, tableData, i, textValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    table = document.getElementById('myTable');
-    tableRow = table.getElementsByTagName("tr");
-    for(i=0; i<tableRow.length; i++){
-        tableData = tableRow[i].getElementsByTagName("td")[0];
-        if(tableData){
-            textValue = tableData.textContent || tableData.innerText;
-            if(textValue.toUpperCase().indexOf(filter) > -1){
-                tableRow[i].style.display = "";
-            }
-            else{
-                tableRow[i].style.display = "none";
-            }
+// FUNCTION FOR SHOWING TABLE USING FOREACH 
+
+function showTable() {
+var tbody1 = document.querySelector("tbody");
+tbody1.innerHTML = "";
+var tableData = ``;
+
+information.forEach((info) => {
+    tableData += `<tr>
+                <td>${info.id}</td>
+                <td>${info.name}</td>
+                <td>${info.city}</td>
+                <td>${info.branch}</td>
+                <td>${info.degree}</td>
+                <td id="${info.id}" onclick="editItem(this)"><button>Edit</button></td>
+                <td id="${info.id}" onclick="deleteItem(this)"><button>Delete</button></td>
+            </tr>` 
+});
+tbody1.innerHTML = tableData
+}
+
+// FUNCTION FOR DELETE
+
+function deleteItem(data){
+var updated_array = information.filter((element)=> {
+return element.id != data.id;
+});
+information = updated_array;
+showTable();
+}
+
+// FUNCTION FOR SEARCH BY ID
+
+
+function searchById(){
+var input, filter, table, tableRow, tableData, i, textValue;
+input = document.getElementById('idInput');
+filter = input.value.toUpperCase();
+table = document.getElementById('myTable');
+tableRow = table.getElementsByTagName("tr");
+for(i=0; i<tableRow.length; i++){
+    tableData = tableRow[i].getElementsByTagName("td")[0];
+    if(tableData){
+        textValue = tableData.textContent || tableData.innerText;
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            tableRow[i].style.display = "";
+        }
+        else{
+            tableRow[i].style.display = "none";
         }
     }
 }
+}
 
-function deleteRow(row)
-{
-    var i=row.parentNode.parentNode.rowIndex;
-    document.getElementById('myTable').deleteRow(i-1);
+// FUNCTION FOR SEARCH BY NAME
+
+function searchByName(){
+var input, filter, table, tableRow, tableData, i, textValue;
+input = document.getElementById('nameInput');
+filter = input.value.toUpperCase();
+table = document.getElementById('myTable');
+tableRow = table.getElementsByTagName("tr");
+for(i=0; i<tableRow.length; i++){
+    tableData = tableRow[i].getElementsByTagName("td")[1];
+    if(tableData){
+        textValue = tableData.textContent || tableData.innerText;
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            tableRow[i].style.display = "";
+        }
+        else{
+            tableRow[i].style.display = "none";
+        }
+    }
+}
+}
+
+// FUNCTION FOR SEARCH BY CITY
+
+
+function searchByCity(){
+var input, filter, table, tableRow, tableData, i, textValue;
+input = document.getElementById('cityInput');
+filter = input.value.toUpperCase();
+table = document.getElementById('myTable');
+tableRow = table.getElementsByTagName("tr");
+for(i=0; i<tableRow.length; i++){
+    tableData = tableRow[i].getElementsByTagName("td")[2];
+    if(tableData){
+        textValue = tableData.textContent || tableData.innerText;
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            tableRow[i].style.display = "";
+        }
+        else{
+            tableRow[i].style.display = "none";
+        }
+    }
+}
+}
+
+// FUNCTION FOR SEARCH BY DEPARTMENT
+
+
+function searchByDept(){
+var input, filter, table, tableRow, tableData, i, textValue;
+input = document.getElementById('deptInput');
+filter = input.value.toUpperCase();
+table = document.getElementById('myTable');
+tableRow = table.getElementsByTagName("tr");
+for(i=0; i<tableRow.length; i++){
+    tableData = tableRow[i].getElementsByTagName("td")[3];
+    if(tableData){
+        textValue = tableData.textContent || tableData.innerText;
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            tableRow[i].style.display = "";
+        }
+        else{
+            tableRow[i].style.display = "none";
+        }
+    }
+}
+}
+
+// FUNCTION FOR SEARCH BY DEGREE
+
+
+function searchByDegree(){
+var input, filter, table, tableRow, tableData, i, textValue;
+input = document.getElementById('degreeInput');
+filter = input.value.toUpperCase();
+table = document.getElementById('myTable');
+tableRow = table.getElementsByTagName("tr");
+for(i=0; i<tableRow.length; i++){
+    tableData = tableRow[i].getElementsByTagName("td")[4];
+    if(tableData){
+        textValue = tableData.textContent || tableData.innerText;
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+            tableRow[i].style.display = "";
+        }
+        else{
+            tableRow[i].style.display = "none";
+        }
+    }
+}
 }
